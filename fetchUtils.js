0485f8a9f2cc818792159e28daf02dea97b8b402
Jacegk1999 <Jacegk1999@gmail.com>
2022-02-10 15:09:12 -0800

    made a fetch file with supabase info

---
+++ fetchUtils.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = 'https://edmczxuwjcwwnsmabezz.supabase.co';
+const SUPABASE_KEY =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVkbWN6eHV3amN3d25zbWFiZXp6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDQzNDE0MjEsImV4cCI6MTk1OTkxNzQyMX0.HouVg2N-Lni_g52lrL1Jla7gOhh61sUGEGckeyC8hnE';
+const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+function checkError({ data, error }) {
+    return error ? console.error(error) : data;
+}
