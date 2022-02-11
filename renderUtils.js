4a7ed80e23cd2bd88e195a70464e1e39a78cd295
Jacegk1999 <Jacegk1999@gmail.com>
2022-02-10 16:44:55 -0800

    added render list item

---
+++ renderUtils.js
@@ -0,0 +1,19 @@
+export function renderListItem(food) {
+    const div = document.createElement('div');
+    div.classList.add('food');
+
+    const a = document.createElement('a');
+    a.href = `./food/?id=${food.id}`;
+
+    const img = document.createElement('img');
+    img.src = `./assets/${food.name}.png`;
+
+    const nameSpan = document.createElement('span');
+    nameSpan.textContent = food.name;
+    nameSpan.classList.add('name');
+
+
+    a.append(img, nameSpan);
+    div.append(a);
+    return div;
+}
