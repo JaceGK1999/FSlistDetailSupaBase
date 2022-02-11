073cfbe18856d6b110c5e14337ea827d80ad88ec
Jacegk1999 <Jacegk1999@gmail.com>
2022-02-10 17:20:18 -0800

    moved html

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 // import functions and grab DOM elements
+import { renderListItem } from './render-utils.js';
+import { getFoodById } from './fetchUtils.js';
 
+const main = document.querySelector('main');
+
+for (let food of resp) {
+    getFoodById();
+    const foodDiv = renderListItem(food);
+    main.append(foodDiv);
+}
 // let state
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+// set event listeners
+// get user input
+// use user input to update state
+// update DOM to reflect the new state
